7b637c38ff8c0e2dcd6660529a7e20cc5bbf0c74
mohamed fathy <116851979+rag-nar1@users.noreply.github.com>
2022-11-19 10:30:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def read_file(file):
 # Create chrome driver
 def createDriver():
     chrome_options = webdriver.ChromeOptions()
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-notifications")
